e54bab9da01378aae17d561e005bc2ef74a4f11e
A-Egoist <1012738513@qq.com>
2025-06-21 17:22:27 +0800

    Remove timer

--- server.cpp
+++ server.cpp
@@ -2,7 +2,6 @@
 
 constexpr int MAX_EVENTS = 1024;  // epoll 每次最多返回的事件数量
 constexpr int READ_BUFFER = 4096;  // 每个 socket 读取数据时的缓冲区大小
-constexpr int KEEP_ALIVE_TIMEOUT = 60000;  // 5 秒
 
 // 构造函数中只是初始化端口号和一些成员变量，listen_fd_ 和 epoll_fd_ 暂时设为无效值。
 WebServer::WebServer(int port) : port_(port), listen_fd_(-1), epoll_fd_(-1), mysql() {}
@@ -52,7 +51,6 @@ void WebServer::initSocket() {
     epoll_event event{};
     event.data.fd = listen_fd_;
     event.events = EPOLLIN | EPOLLET;  // 可读 + 边缘触发
-    // event.events = EPOLLIN | EPOLLET | EPOLLONESHOT;  // 可读 + 边缘触发
     epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, listen_fd_, &event);
 }
 
@@ -112,10 +110,6 @@ std::string receiveHttpRequest(int client_fd) {
     return raw_data;
 }
 
-void WebServer::handleGET(HttpRequest& request, int client_fd) {
-
-}
-
 void parseFormURLEncoded(const std::string& body, std::unordered_map<std::string, std::string>& data) {
     std::istringstream stream(body);
     std::string pair;
@@ -161,55 +155,11 @@ bool WebServer::handlePOST(HttpRequest& request, int client_fd) {
 }
 
 void WebServer::handleConnection(int client_fd) {
-    char buffer[READ_BUFFER];
-    static std::unordered_map<int, std::string> read_buffers;
+    std::string raw_data = receiveHttpRequest(client_fd);
+    HttpRequest request = parseHttpRequest(raw_data);
 
-    while (true) {
-        int bytes_read = recv(client_fd, buffer, sizeof(buffer), 0);
-        if (bytes_read < 0) {
-            if (errno == EAGAIN || errno == EWOULDBLOCK) break;
-            else {
-                closeClient(client_fd);
-                return;
-            }
-        } else if (bytes_read == 0) {
-            // 对方关闭连接
-            closeClient(client_fd);
-            return;
-        }
-
-        // 累积读取数据
-        read_buffers[client_fd].append(buffer, bytes_read);
-    }
-
-    std::string& data = read_buffers[client_fd];
-
-    while (true) {
-        HttpRequest request;
-        size_t consumed = 0;
-
-        // 尝试解析完整请求；若不完整则 break 等待下一次数据到来
-        if (!tryParseHttpRequest(data, request, consumed)) break;
-
-        processHttpRequest(client_fd, request);
-        data.erase(0, consumed);
-
-        // 非持久连接，关闭
-        if (request.headers.count("Connection") && request.headers["Connection"] == "close") {
-            closeClient(client_fd);
-            read_buffers.erase(client_fd);
-            return;
-        }
-
-        // 持久连接：刷新定时器
-        addOrUpdateTimer(client_fd, KEEP_ALIVE_TIMEOUT);
-    }
-
-    epoll_event event{};
-    event.data.fd = client_fd;
-    event.events = EPOLLIN | EPOLLET;
-    // event.events = EPOLLIN | EPOLLET | EPOLLONESHOT;
-    epoll_ctl(epoll_fd_, EPOLL_CTL_MOD, client_fd, &event);
+    processHttpRequest(client_fd, request);
+    closeClient(client_fd);
 }
 
 
@@ -228,11 +178,12 @@ void WebServer::run() {
             perror("epoll_wait failed");
             break;
         }
-
+        
         // 遍历请求队列中的每一个 Connection
         for (int i = 0; i < nfds; ++ i) {
             int fd = events[i].data.fd;
             if (fd == listen_fd_) {
+                // 接收新连接, 持续接收, 直至没有新的连接到达
                 while (true) {
                     sockaddr_in client_addr{};
                     socklen_t client_len = sizeof(client_addr);
@@ -245,108 +196,54 @@ void WebServer::run() {
                     epoll_event event{};
                     event.data.fd = client_fd;
                     event.events = EPOLLIN | EPOLLET;
-                    // event.events = EPOLLIN | EPOLLET | EPOLLONESHOT;
                     epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, client_fd, &event);
-
-                    // 添加定时器（初始超时时间）
-                    addOrUpdateTimer(client_fd, KEEP_ALIVE_TIMEOUT);
                 }
             } else {
+                // 处理客户端数据
                 handleConnection(fd);
             }
         }
-        processCloseQueue();
     }
 
     close(listen_fd_);
     close(epoll_fd_);
 }
 
-void WebServer::processCloseQueue() {
-    std::lock_guard<std::mutex> lock(close_queue_mutex_);
-    while (!close_queue_.empty()) {
-        int fd = close_queue_.front();
-        close_queue_.pop();
-        closeClient(fd);
-    }
-}
-
 void WebServer::closeClient(int client_fd) {
     epoll_ctl(epoll_fd_, EPOLL_CTL_DEL, client_fd, nullptr);
     close(client_fd);
-
-    if (timers_.count(client_fd)) {
-        timers_[client_fd]->cancel();
-        timers_.erase(client_fd);
-    }
     Logger::getInstance().log("INFO", "Client[" + std::to_string(client_fd) + "] closed");
 }
 
 void WebServer::processHttpRequest(int client_fd, HttpRequest& request) {
     // 根据客户端请求的 URL 路径，动态返回不同的页面内容
-    std::string resources_root_path = "/home/amonologue/Projects/WebServer/resources";
-    std::string file_path;
-    std::string response_body;
-    std::string content_type;
-    std::string status_line;
-
-    // 路由匹配
-    bool main_path = true;
-    if (request.path == "/") {
-        file_path = resources_root_path + "/index.html";
-    } else if (request.path == "/picture") {
-        file_path = resources_root_path + "/picture.html";
-    } else if (request.path == "/video") {
-        file_path = resources_root_path + "/video.html";
-    } else if (request.path == "/login") {
-        file_path = resources_root_path + "/login.html";
-    } else if (request.path == "/register") {
-        file_path = resources_root_path + "/register.html";
-    } else if (request.path == "/welcome") {
-        file_path = resources_root_path + "/welcome.html";
-    } else {
-        file_path = resources_root_path + request.path;
-        main_path = false;
-    }
-
-    bool is_redirect = false;
+    std::string file_path = router(request.path);
+    
+    // POST
     if (request.method == "POST") {
         bool success = handlePOST(request, client_fd);
         if (success) {
-            is_redirect = true;  // 设置重定向 flag
+            std::string redirect_response = "HTTP/1.1 302 Found\r\nLocation: /welcome\r\nContent-Length: 0\r\nConnection: close\r\n";
+            send(client_fd, redirect_response.c_str(), redirect_response.size(), 0);  // 发送重定向响应
+            return ;
+        } else {
+            // TODO, Incorrect username or password;
         }
     }
 
+    // GET
+    std::string status_line;
+    std::string response_body;
+    std::string content_type;
     std::ifstream file(file_path, std::ios::binary);
-    if (is_redirect) {
-        status_line = "HTTP/1.1 302 Found\r\n";
-        std::string redirect_location = "/welcome";  // 重定向到 welcome 界面
-
-        std::string redirect_response = 
-            status_line + 
-            "Location: " + redirect_location + "\r\n" + 
-            "Content-Length: 0\r\n" + 
-            "Connection: keep-alive\r\n" + 
-            "Keep-Alive: timeout=5\r\n\r\n";
-        if (KEEP_ALIVE_TIMEOUT == 0) {
-            redirect_response = 
-                status_line + 
-                "Location: " + redirect_location + "\r\n" + 
-                "Content-Length: 0\r\n" + 
-                "Connection: close\r\n\r\n";
-        }
-        send(client_fd, redirect_response.c_str(), redirect_response.size(), 0);  // 发送响应
-        if (main_path) Logger::getInstance().log("INFO", "Response to Client[" + std::to_string(client_fd) + "]");
-        return ;
-    }
-
+    
     if (file) {
         status_line = "HTTP/1.1 200 OK\r\n";
         response_body = readFile(file_path);
         content_type = getContentType(file_path);
     } else {
         status_line = "HTTP/1.1 404 Not Found\r\n";
-        response_body = readFile(resources_root_path + "/404.html");
+        response_body = readFile("/home/amonologue/Projects/WebServer/resources/404.html");
         content_type = "text/html";
     }
 
@@ -354,83 +251,30 @@ void WebServer::processHttpRequest(int client_fd, HttpRequest& request) {
         status_line + 
         "Content-Type: " + content_type + "\r\n" + 
         "Content-Length: " + std::to_string(response_body.size()) + "\r\n" + 
-        "Connection: keep-alive\r\n" + 
-        "Keep-Alive: timeout=5\r\n\r\n" + 
+        "Connection: close\r\n\r\n" + 
         response_body;
-    if (KEEP_ALIVE_TIMEOUT == 0) {
-        response = 
-            status_line + 
-            "Content-Type: " + content_type + "\r\n" + 
-            "Content-Length: " + std::to_string(response_body.size()) + "\r\n" + 
-            "Connection: close\r\n\r\n" + 
-            response_body;
-    }
     
     send(client_fd, response.c_str(), response.size(), 0);  // 发送响应
-    if (main_path) Logger::getInstance().log("INFO", "Response to Client[" + std::to_string(client_fd) + "]");
 }
 
-void WebServer::addOrUpdateTimer(int client_fd, int timeout_ms) {
-    if (timers_.count(client_fd)) {
-        timers_[client_fd]->reset(timeout_ms);
+std::string WebServer::router(std::string& requestPath) {
+    std::string resources_root_path = "/home/amonologue/Projects/WebServer/resources";
+    std::string file_absolute_path;
+    // 路由匹配
+    if (requestPath == "/") {
+        file_absolute_path = resources_root_path + "/index.html";
+    } else if (requestPath == "/picture") {
+        file_absolute_path = resources_root_path + "/picture.html";
+    } else if (requestPath == "/video") {
+        file_absolute_path = resources_root_path + "/video.html";
+    } else if (requestPath == "/login") {
+        file_absolute_path = resources_root_path + "/login.html";
+    } else if (requestPath == "/register") {
+        file_absolute_path = resources_root_path + "/register.html";
+    } else if (requestPath == "/welcome") {
+        file_absolute_path = resources_root_path + "/welcome.html";
     } else {
-        // auto timer = std::make_shared<TimerTask>(
-        //     client_fd, timeout_ms, [client_fd]() {
-        //         Logger::getInstance().log("INFO", "Client[" + std::to_string(client_fd) + "] timed out");
-        //         // closeClient(client_fd);
-        //         close(client_fd);
-        //     });
-        auto timer = std::make_shared<TimerTask>(
-            client_fd, timeout_ms, [this, client_fd]() {
-                Logger::getInstance().log("INFO", "Client[" + std::to_string(client_fd) + "] timed out");
-                std::lock_guard<std::mutex> lock(this->close_queue_mutex_);
-                this->close_queue_.push(client_fd);
-            });
-
-        timers_[client_fd] = timer;
-        timer->start();
+        file_absolute_path = resources_root_path + requestPath;
     }
-}
-
-bool tryParseHttpRequest(const std::string& raw, HttpRequest& request, size_t& request_len) {
-    size_t header_end = raw.find("\r\n\r\n");
-    if (header_end == std::string::npos) return false;  // 说明 header 还没收完整
-
-    std::istringstream stream(raw.substr(0, header_end + 2));  // 含最后一个空行
-    std::string line;
-    ParseState state = ParseState::REQUEST_LINE;
-
-    while (std::getline(stream, line)) {
-        if (!line.empty() && line.back() == '\r') line.pop_back();
-
-        switch (state) {
-            case ParseState::REQUEST_LINE:
-                parseRequestLine(line, request);
-                state = ParseState::HEADERS;
-                break;
-            case ParseState::HEADERS:
-                if (line.empty()) {
-                    state = ParseState::BODY;
-                } else {
-                    parseHeaderLine(line, request);
-                }
-                break;
-            default:
-                break;
-        }
-    }
-
-    size_t content_length = 0;
-    if (request.headers.count("Content-Length")) {
-        content_length = std::stoi(request.headers["Content-Length"]);
-    }
-
-    size_t total_len = header_end + 4 + content_length;
-    if (raw.size() < total_len) return false;  // 还没收到完整 body
-
-    if (content_length > 0)
-        request.body = raw.substr(header_end + 4, content_length);
-
-    request_len = total_len;
-    return true;
+    return file_absolute_path;
 }
--- server.hpp
+++ server.hpp
@@ -12,7 +12,6 @@
 #include "http/http_request.hpp"
 #include "sql/MySQLConnector.hpp"
 #include "log/log.hpp"
-#include "timer/timer_task.hpp"
 
 class WebServer {
 public:
@@ -25,29 +24,21 @@ private:
     int listen_fd_;  // 
     int epoll_fd_;  // 
     MySQLConnector mysql;
-    // TimerManager timer_mgr_;
-    std::unordered_map<int, std::shared_ptr<TimerTask>> timers_;
-    std::queue<int> close_queue_;
-    std::mutex close_queue_mutex_;
 
     void initSocket();
     void handleConnection(int client_fd);
     void setNonBlocking(int fd);
-    void handleGET(HttpRequest&, int);
-    bool handlePOST(HttpRequest&, int);
+    bool handlePOST(HttpRequest& request, int client_fd);
     void processHttpRequest(int client_fd, HttpRequest& request);
-    void addOrUpdateTimer(int client_fd, int timeout_ms);
-    void processCloseQueue();
+    std::string router(std::string& requestPath);
 };
 
-std::string getContentType(const std::string&);
+std::string getContentType(const std::string& path);
 
-std::string readFile(const std::string&);
+std::string readFile(const std::string& file_path);
 
-std::string receiveHttpRequest(int);
+std::string receiveHttpRequest(int client_fd);
 
-bool tryParseHttpRequest(const std::string& raw, HttpRequest& request, size_t& request_len);
+void parseFormURLEncoded(const std::string& body, std::unordered_map<std::string, std::string>& data);
 
-void parseFormURLEncoded(const std::string&, std::unordered_map<std::string, std::string>&);
-
-std::string decodeURLComponent(const std::string&);
+std::string decodeURLComponent(const std::string& s);
